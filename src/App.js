a7b5bb59299a1fd430044849fba3c24a314ce9f8
Zeta <ezcodemagic@gmail.com>
2019-08-22 18:31:47 -0400

    removed radium after testing that method for styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+
 import Person from './Person/Person';
 
 
@@ -64,10 +64,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -103,17 +99,17 @@ class App extends Component {
     
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={ classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={() => this.togglePersonsHandler('Maximilian!!')}>Switch Name</button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={ classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={() => this.togglePersonsHandler('Maximilian!!')}>Switch Name</button>
+          {persons}
+        </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
