a30a820927dfd6377ac7342b699ad57329c3a1e8
Zeta <ezcodemagic@gmail.com>
2019-08-23 18:04:11 -0400

    applied css modules instead of radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
 
@@ -89,19 +89,19 @@ class App extends Component {
       }
     }
 
-    let classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red'); 
+      assignedClasses.push( classes.red ); 
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
     
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={ classes.join(' ')}>This is really working!</p>
+          <p className={ assignedClasses.join(' ')}>This is really working!</p>
           <button 
             style={style}
             onClick={() => this.togglePersonsHandler('Maximilian!!')}>Switch Name</button>
